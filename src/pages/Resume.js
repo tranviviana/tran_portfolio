6cea92826d2f3be8c88f85f4fc471841bdecb839
Viviana Tran <tranviviana@berkeley.edu>
2024-07-01 15:56:16 -0700

    resume page and sticky navbar

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,14 @@
 import React from "react";
-// updated resume can be found here: (change file path to update file )
-import myResume from "../images/generalImages/tranviviana.pdf"
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import myResume from "../images/generalImages/tranviviana.pdf";
+
+
 export default function Resume() {
-    //in the future, edit so that links in the resume are clickable
     return (
-        <div className='resume'>
-            <h2>Experience In A Glance</h2>
-            <Document
-            file={myResume}
-            onLoadError={console.error}
-            style={{ width: '100vw', height: 'auto'}}
-            >
-              <Page pageIndex={0}/>
-            </Document>
+        <>
+        <h2>Experience In A Glance</h2>
+        <div className="resume-div">
+        <embed src={myResume} className='resume'/>
         </div>
-    )
+        </>
+    );
 }

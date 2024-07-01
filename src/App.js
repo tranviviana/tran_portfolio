115ea35bb385db72888b0c48d4ac35ecf64ac743
Viviana Tran <tranviviana@berkeley.edu>
2024-06-30 23:14:58 -0700

    added gitignore

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <>
       <div className={`${darkMode ? 'dark-mode' : ''}`}>
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      </div>
+      
     <div className="container">
       <Routes>
       <Route path="/" element={<MainPage />}/>
@@ -30,7 +30,7 @@ function App() {
       <Route path="/resume" element={<Resume />}/>
       </Routes>
     </div>
-    
+    </div>
     
     </>
   );

ba15c9c7138e50a7a7c00d5dab7102c4eabfc3bd
Viviana Tran <tranviviana@berkeley.edu>
2024-07-01 18:57:37 -0700

    hero section but need to fix resume section

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,10 +6,13 @@ export default function MainPage() {
         <>
         <div className="hero-section">
             <div className='hero-left'>
-                <h1 className='special-txt'>I'm 
-                <h1 >Viviana</h1></h1>
+            <div className='name-container'>
+                    <h1 className='special-txt'>I'm </h1>
+                    <h1> Viviana</h1>
+                </div>
                 <h3>Student | Front-End Developer | Programmer | Electrical Engineer</h3>
                 <Link to="/resume" className="navbar_txt outline">Resume</Link>
+                <h6 style={{fontStyle: 'italic', fontWeight: '400'}}>Update: Developing Software for ECG Analysis, Exploring the Vast Field of Technology, and Continuing Work at theCoderSchool</h6>
             </div>
             <div className='hero-right'>
                 <FlipImage/>

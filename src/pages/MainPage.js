9ac251622a763531c0106313570181b3ad4106c4
Viviana Tran <tranviviana@berkeley.edu>
2024-07-01 22:20:49 -0700

    bruh

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,25 +1,9 @@
 import React from "react"
-import FlipImage from "../components/FlipImage"
-import {Link} from "react-router-dom"
+import HeroSection from "../sections/HeroSection"
 export default function MainPage() {
     return (
         <>
-        <div className="hero-section">
-            <div className='hero-left'>
-            <div className='name-container'>
-                    <h1 className='special-txt'>I'm </h1>
-                    <h1> Viviana</h1>
-                </div>
-                <h3>Student | Front-End Developer | Programmer | Electrical Engineer</h3>
-                <h6 style={{fontStyle: 'italic', fontWeight: '400'}}>Currently: Developing Software for ECG Analysis, Exploring the Vast Concentrations Within Technology, and Continuing Work at theCoderSchool</h6>
-                <div className='hero-resume-div'>
-                <Link to="/resume" className="hero-resume-btn" >Resume</Link>
-                </div>
-            </div>
-            <div className='hero-right'>
-                <FlipImage/>
-            </div>
-        </div>
+        <HeroSection/>
         </>
     )
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,13 +1,17 @@
 import React from "react";
 import myResume from "../images/generalImages/tranviviana.pdf";
-
+{/*Resume Page stylings in styles.css*/ }
 
 export default function Resume() {
     return (
         <>
+        <div className="resume-container">
+        <div>
         <h2>Experience In A Glance</h2>
+        </div>
         <div className="resume-div">
         <embed src={myResume} className='resume'/>
+        </div>
         </div>
         </>
     );

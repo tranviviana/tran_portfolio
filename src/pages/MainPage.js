c54007f2d358e227890d40b5c4a318f675b0cfc4
Viviana Tran <tranviviana@berkeley.edu>
2024-07-03 12:06:25 -0700

    modal not working properly

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,12 @@
 import React from "react"
 import HeroSection from "../sections/HeroSection"
+import ContactMe from "../sections/ContactMe"
+
 export default function MainPage() {
     return (
         <>
         <HeroSection/>
+        <ContactMe/>
         </>
     )
 }
--- src/sections/ContactMe.js
+++ src/sections/ContactMe.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import 'ContactMe.css';
-import gmail from './gmail.png';
-import linkedin from './linkedin.png';
-import github from './github.png';
-{/* link github as well */}
+import './ContactMe.css';
+import gmail from '../images/contactMe/gmail.png';
+import linkedin from '../images/contactMe/linkedin.png';
+import github from '../images/skills/github.png';
+import Modal from '../components/Modal';
+
 {/*modal with url and then click on to send*/}
 export default function ContactMe() {
-    <Modal></Modal>
+    return (
+        <>
+        <h1>Contact Me</h1>
+        <div className='ContactModals'>
+            <Modal open_text='tranviviana@berkeley.edu' closed_image={gmail} />
+            <Modal open_text='https://www.linkedin.com/in/viviana-tran' closed_image={linkedin} />
+            <Modal open_text='https://github.com/tranviviana' closed_image={github} />
+        </div>
+        </>
+    )
 }

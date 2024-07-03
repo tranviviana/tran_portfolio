71f8e5d0bee71c95986fc25afbac7ae6a52a1058
Viviana Tran <tranviviana@berkeley.edu>
2024-07-03 12:22:00 -0700

    modal for contact but not easing

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
+import './Modal.css';
+{/*Fix ease of the modal in the future*/}
+export default function Modal({ closed_image, open_text, link_text }) {
+    const [isOpen, setIsOpen] = useState(false);
 
-export default function Modal({ open_text, closed_image }) {
+    const handleOpenModal = () => {
+        setIsOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div>
-            <button data-modal-target="#modal">
+            <button onClick={handleOpenModal}>
                 <img src={closed_image} alt="Closed Modal" />
             </button>
-            <div className="modal" id="modal">
-                <div className="modal-header">
-                    <div className="title">URL</div>
-                    <button data-close-button className="close-button">&times;</button>
+            {isOpen && (
+                <div className={`modal ${isOpen ? 'open' : ''}`} id="modal">
+                    <div className="modal-header">
+                        <div className="title">URL</div>
+                        <button onClick={handleCloseModal} className="close-button">&times;</button>
+                    </div>
+                    <div className="modal-body">
+                        {open_text}
+                        <a href={link_text} target="_blank" rel="noopener noreferrer">
+                            <button>Send Me There</button>
+                        </a>
+                    </div>
                 </div>
-                <div className="modal-body">
-                    {open_text}
-                    <a href={open_text} target="_blank" rel="noopener noreferrer">
-                        <button>Send Me There</button>
-                    </a>
-                </div>
-            </div>
-            <div id="overlay"></div>
+            )}
+            {isOpen && <div className={`overlay ${isOpen ? 'open' : ''}`} id="overlay"></div>}
         </div>
     );
 }
--- src/sections/ContactMe.js
+++ src/sections/ContactMe.js
@@ -11,9 +11,9 @@ export default function ContactMe() {
         <>
         <h1>Contact Me</h1>
         <div className='ContactModals'>
-            <Modal open_text='tranviviana@berkeley.edu' closed_image={gmail} />
-            <Modal open_text='https://www.linkedin.com/in/viviana-tran' closed_image={linkedin} />
-            <Modal open_text='https://github.com/tranviviana' closed_image={github} />
+            <Modal open_text='tranviviana@berkeley.edu' closed_image={gmail} link_text='mailto:tranviviana@berkeley.edu' />
+            <Modal open_text='https://www.linkedin.com/in/viviana-tran' closed_image={linkedin} link_text='https://www.linkedin.com/in/viviana-tran' />
+            <Modal open_text='https://github.com/tranviviana' closed_image={github} link_text='https://github.com/tranviviana'/>
         </div>
         </>
     )

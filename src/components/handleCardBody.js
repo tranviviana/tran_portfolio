c819814156a8178e0aea5eceae4742a7f0a5f8cb
Viviana Tran <tranviviana@berkeley.edu>
2024-07-03 22:17:45 -0700

    forgot to save this one

---
+++ src/components/handleCardBody.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+export default function handleCardBody() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+    };
+    return (
+        <
+    )
+}

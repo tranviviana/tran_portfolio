8981c302809fba95c3057b253414e50419c2bd81
Viviana Tran <tranviviana@berkeley.edu>
2024-07-05 14:54:54 -0700

    img not showing :(

--- src/components/handleCardBody.js
+++ src/components/handleCardBody.js
@@ -1,6 +1,33 @@
 import React, { useState } from 'react';
 import './HandleCardBody.css'
 import CardBody from './CardBody.js';
+import runbutton from '../images/projectPics/runbutton.png'
+import pong from '../images/projectPics/pong.png'
+import rockpaperscissors from '../images/projectPics/rockpaperscissors.png'
+import rotatingbutton from '../images/projectPics/rotatingbutton.png'
+import youtubeclone from '../images/projectPics/youtubeclone.png'
+import kachow from '../images/projectPics/kachow.png'
+import cpu from '../images/projectPics/cpu.png'
+import riscv from '../images/projectPics/riscv.png'
+import snek from '../images/projectPics/snek.png'
+import grocereyes from '../images/projectPics/grocereyes.jpg'
+import voicecar from '../images/projectPics/voicecar.jpg'
+import mazegame from '../images/projectPics/mazegame.png'
+import ngrams from '../images/projectPics/ngrams.png'
+import deque from '../images/projectPics/deque.png'
+import twozero from '../images/projectPics/2048.png'
+import bioehswebsite from '../images/projectPics/bioehswebsite.png'
+import hcc from '../images/projectPics/hcc.jpg'
+import teetertoggle from '../images/projectPics/teetertoggle.jpg'
+import scheme from '../images/projectPics/scheme.png'
+import ants from '../images/projectPics/ants.png'
+import cats from '../images/projectPics/cats.png'
+import hog from '../images/projectPics/hog.png'
+import aps from '../images/projectPics/aps.png'
+import touchscreen from '../images/projectPics/touchscreen.jpg'
+import imgrecon from '../images/projectPics/imagereconstruction.jpg'
+import matlab from '../images/projectPics/matlab.jpg'
+import seeingnightmares from '../images/projectPics/seeingnightmares.png'
 
 
 
@@ -33,14 +60,14 @@ export default function HandleCardBody({ items, image, name }) {
                         </div>
                         <div className='career-modal-body'>
                         {filteredItems.map(item => (
-                                <div key={item.id} className='project-holder'>
+                                <div key={item.column1} className='project-holder'>
                                     
                                     <h2>{item.nameOfProject}</h2>
                                     <div className='info'>
                                         <h3>Task: </h3>
                                         <p>{item.task}</p>
                                     </div>
-                                    <img src={item.picture} alt={item.nameOfProject} className='project-picture'/>
+                                    <img src={item.pictures} alt={item.nameOfProject} className='project-picture'/>
                                     <div className='info'>
                                         <h3>Challenges Encountered: </h3>
                                         <p>{item.challenges}</p>

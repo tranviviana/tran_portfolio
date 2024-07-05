a826e5667e4cb24f4f959274438a5ffabb2376fd
Viviana Tran <tranviviana@berkeley.edu>
2024-07-05 09:50:21 -0700

    skills section done

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -4,20 +4,27 @@ import java from '../images/skills/java.png';
 import python from '../images/skills/python.png';
 import react from '../images/skills/react.png';
 import riscv from '../images/skills/risc-v.png';
+import arduino from '../images/skills/arduino.png';
+import c from '../images/skills/c.png';
+import empty from '../images/skills/empty.png';
+import logism from '../images/skills/logism.png';
 import './Skills.css';
+import vscode from '../images/skills/vscode.png';
+import intellij from '../images/skills/intellij.png';
 
-const images = [figma, github, java, python, react, riscv];
+const images = [figma, github, java, python, react, riscv, arduino, c, logism, vscode, intellij];
 
 export default function Skills() {
     return (
         <div className="skills-container">
             <div><h1>Skills</h1></div>
-            <div id='skill-images-container'>
+            <div className='skill-images-container'>
             {images.map((image, index) => (
                 <div key={index}>
                     <img src={image} alt="Skill" className='skill-images' />
                 </div>
             ))}
+            <h3 style={{ fontWeight: 'lighter' }} className='skill-images'>+Others</h3>
             </div>
         </div>
     )

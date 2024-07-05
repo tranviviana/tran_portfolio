2a60cd1b46b6cb2635c87c38261f5b81ca68cb86
Viviana Tran <tranviviana@berkeley.edu>
2024-07-05 11:29:02 -0700

    changing titles

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -10,8 +10,8 @@ export default function AboutMe() {
         <div className='about-me-styling'>
         <h1>About Me</h1>
         <div className='about-me-cards'>
-        <HandleCardBody items={projectdata} image={cs} name='Software'/>
-        <HandleCardBody items={projectdata} image={ee} name='Electrical Engineering'/>
+        <HandleCardBody items={projectdata} image={cs} name='Software Engineer (General)'/>
+        <HandleCardBody items={projectdata} image={ee} name='Electrical Engineer'/>
         <HandleCardBody items={projectdata} image={frontend} name='Front-End Developer'/>
         <HandleCardBody items={projectdata} image={innovator} name='Innovator'/>
         </div>

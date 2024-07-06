7496f5cc9db834d6a04cd130eb04cfeda8079892
Viviana Tran <tranviviana@berkeley.edu>
2024-07-05 19:02:44 -0700

    re added

---
+++ src/components/HandleCardBody.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import './HandleCardBody.css'
+import CardBody from './CardBody';
+
+
+
+
+export default function HandleCardBody({ items, image, name }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsOpen(true);
+        
+    };
+
+    const handleCloseModal = (e) => {
+        if (e.target.id === 'overlay' || e.target.className === 'close-button') {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleCloseModal);
+        return () => {
+            document.removeEventListener('click', handleCloseModal);
+        };
+    }, []);
+    const itemArray = Array.from(items);
+    const filteredItems = itemArray.filter(item => item.groupings === name);
+    
+
+    return (
+        
+            <div className='career-categories'>
+            <button onClick={handleOpenModal} className='cards'>
+            <CardBody image={image} name={name}/>
+            </button>
+                {isOpen && (
+                    <div className={`career-modal ${isOpen ? 'open' : ''}`} id="career-modal">
+                        <div className='career-modal-header'>
+                            <h2>{name}</h2>
+                            <button onClick={handleCloseModal} className="close-button">&times;</button>
+                        </div>
+                        <div className='career-modal-body'>
+                        {filteredItems.map(item => (
+                                <div key={item.column1} className='project-holder'>
+                                    
+                                    <h2>{item.nameOfProject}</h2>
+                                    <div className='info'>
+                                        <h3>Task: </h3>
+                                        <p>{item.task}</p>
+                                    </div>
+                                    <embed src={item.pictures} className='project-picture'/>
+                                    <div className='info'>
+                                        <h3>Challenges Encountered: </h3>
+                                        <p>{item.challenges}</p>
+                                    </div>
+                                    <div className='info'>
+                                        <h3>Process and Outcome: </h3>
+                                        <p>{item.outcomeuniqueAdditions}</p>
+                                    </div>
+                                    <div className='info'>
+                                    <h3>Link: </h3>
+                                    {item.resource && (
+                                        <a href={item.resource} target="_blank" rel="noopener noreferrer">{item.resource}</a>
+                                        )}
+                                    </div>
+                                    <div className='info'>
+                                        <h3>Topics Used: </h3>
+                                        <p>{item.topicsUsed}</p>
+                                    </div>
+                                    
+                                </div>
+                            ))}
+                            
+                        </div>
+                    </div>
+                )}
+                {isOpen && <div className={`overlay ${isOpen ? 'open' : ''}`} id="overlay"></div>}
+            </div>
+        
+    );
+  }

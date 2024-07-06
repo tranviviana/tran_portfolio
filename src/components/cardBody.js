6988354dbe2bd52d3776b5acfd24f7336805f9a3
Viviana Tran <tranviviana@berkeley.edu>
2024-07-05 19:06:25 -0700

    delete card body

--- src/components/cardBody.js
+++
@@ -1,9 +0,0 @@
-import './CardBody.css';
-export default function CardBody({image, name}){
-    return(
-        <div className='project-card'>
-            <img src={image} alt={name} className='project-card-images'/>
-            <h3>{name}</h3>
-        </div>
-    )
-}

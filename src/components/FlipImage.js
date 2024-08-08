0e8f4e53d5b7974e30734c8820490fd6857f68c3
Viviana Tran <tranviviana@berkeley.edu>
2024-08-08 16:56:08 -0700

    fixing sizing

--- src/components/FlipImage.js
+++ src/components/FlipImage.js
@@ -14,10 +14,10 @@ export default function FlipImage() {
     <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onMouseEnter={handleFlip} onMouseLeave={handleFlip}>
       <div className="flip-card-inner">
         <div className="flip-card-front">
-          <img src={me} alt="Me!" style={{ width: '100%', height: '100%' }}/>
+          <img src={me} alt="Me!" style={{ width: '100%', height: '100%', minWidth: '350px', maxWidth: '425px' }}/>
         </div>
         <div className="flip-card-back">
-          <img src={doggos} alt="My Fur Friends!"  style={{ width: '100%', height: '100%' }}/>
+          <img src={doggos} alt="My Fur Friends!"  style={{ width: '100%', height: '100%', minWidth: '350px',  maxWidth: '425px' }}/>
         </div>
       </div>
     </div>

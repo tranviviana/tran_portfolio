747c971d25d7f7704801dd0ed65a40fe2ca69cc1
Viviana Tran <tranviviana@berkeley.edu>
2024-08-09 14:10:17 -0700

    resume section (uncentered title)

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,17 +1,18 @@
 import React from "react";
 import myResume from "../images/generalImages/tranviviana.pdf";
+import "./Resume.css";
 
 export default function Resume() {
     return (
-        <>
         <div className="resume-container">
-        <div>
-        <h2>Experience In A Glance</h2>
+            <div>
+                <h2>Experience In A Glance</h2>
+            </div>
+            <div className="resume-div">
+                <object data={myResume} type="application/pdf" className="Resume" width="100%" height="600">
+                    <p>This browser does not support PDFs. Please <a href={myResume} download>download the file</a> to view it.</p>
+                </object>
+            </div>
         </div>
-        <div className="resume-div">
-        <embed src={myResume} className='resume'/>
-        </div>
-        </div>
-        </>
     );
 }
